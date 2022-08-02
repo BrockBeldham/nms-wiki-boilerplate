dacfa9a35d09d0063784616dc718eb5279950946
Brock Beldham <bbeldham@unioncreative.com>
2022-08-02 13:04:22 -0400

    🔧 config for gh pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: isProd ? '/your-github-repo-name/' : ''
 }
 
 module.exports = nextConfig
