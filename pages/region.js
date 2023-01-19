2f4ccee7232b422ae77b136e01e548526fa5f8d3
Brock Beldham <bbeldham@unioncreative.com>
2023-01-19 10:10:48 -0500

    ⚡️ moved region template to a seperate component

--- pages/region.js
+++ pages/region.js
@@ -1,8 +1,9 @@
 import { useState, useRef, useReducer } from 'react';
 import Head from 'next/head';
+import reducer from '../reducers';
 import * as ga from '../lib/ga';
 import useCopyToClipboard from '../hooks/copy-to-clipboard';
-import reducer from '../reducers';
+import regionTemplate from '../lib/code-templates/region-template';
 import CodeView from '../components/layouts/code-view';
 import Input from '../components/input';
 import Dropzone from '../components/dropzone';
@@ -27,40 +28,7 @@ export default function Creature() {
   const [viewCode, setViewCode] = useState(false);
   const [data, dispatch] = useReducer(reducer, initialState);
 
-  const codeTemplate = `{{Version|${process.env.NEXT_PUBLIC_VERSION}}}
-{{Region infobox
-| name = ${data.title}
-| image = ${data.image || 'nmsMisc_NotAvailable.png'}
-| galaxy = ${data.galaxy}
-| quadrant = ${data.quadrant}
-| coordinates = ${data.coordinates.split(':')[0] ? data.coordinates.split(':')[0] : 'XXXX'}:${data.coordinates.split(':')[1] ? data.coordinates.split(':')[1] : 'XXXX'}:${data.coordinates.split(':')[2] ? data.coordinates.split(':')[2] : 'XXXX'}:XXXX
-| distance = ${data.distance}
-| civilized = ${data.civ}
-| release = ${process.env.NEXT_PUBLIC_VERSION}
-}}
-'''${data.title}''' is a region.
-
-==Summary==
-'''${data.title}''' is a [[region]] in the [[${data.galaxy}]] [[galaxy]].
-
-==Region Stats==
-{{CARGORegionStats}}
-
-==Documented Systems==
-{{CARGORegionSystems}}
-
-==Other Systems==
-
-==Location==
-This region is approximately ${data.distance || 'XXX,XXX'} light years from the [[Galaxy Centre]] in the ${data.quadrant} [[quadrant]].
-${data.coordinates ? `{{coords|${data.coordinates.split(':')[0] ? data.coordinates.split(':')[0] : 'XXXX'}|${data.coordinates.split(':')[1] ? data.coordinates.split(':')[1] : 'XXXX'}|${data.coordinates.split(':')[2] ? data.coordinates.split(':')[2] : 'XXXX'}|XXXX}}` : ''}
-
-===Adjoining Regions===
-The following regions border ${data.title}:
-{{RegionNeighbours|coord=${data.coordinates.split(':')[0] ? data.coordinates.split(':')[0] : 'XXXX'}:${data.coordinates.split(':')[1] ? data.coordinates.split(':')[1] : 'XXXX'}:${data.coordinates.split(':')[2] ? data.coordinates.split(':')[2] : 'XXXX'}:XXXX|gal=${data.galaxy || 'Euclid'}}}
-
-==Civilized Space==
-${data.civ ? `This region has been explored by the [[${data.civ}]].` : 'This region has not been explored by a [[Civilized space|civilization]]'}`;
+  const codeTemplate = regionTemplate(data);
 
   return (
     <CodeView
