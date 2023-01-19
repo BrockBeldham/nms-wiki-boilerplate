d4e97030a525a580bd9d117fec0dd8820a5b70b0
Brock Beldham <bbeldham@unioncreative.com>
2023-01-19 09:58:29 -0500

    ⚡️ moved civ template to a seperate component

--- pages/civ.js
+++ pages/civ.js
@@ -2,6 +2,7 @@ import { useState, useRef, useReducer } from 'react';
 import Head from 'next/head';
 import * as ga from '../lib/ga';
 import useCopyToClipboard from '../hooks/copy-to-clipboard';
+import civTemplate from '../lib/code-templates/civ-template';
 import reducer from '../reducers';
 import CodeView from '../components/layouts/code-view';
 import Input from '../components/input';
@@ -58,74 +59,7 @@ export default function Civ() {
   const [viewCode, setViewCode] = useState(false);
   const [data, dispatch] = useReducer(reducer, initialState);
 
-  const codeTemplate = `{{Version|${process.env.NEXT_PUBLIC_VERSION}}}
-{{Civilized Space}}
-{{Civilized Space infobox
-| name = ${data.title}
-| image = ${data.image || 'nmsMisc_NotAvailable.png'}
-| galaxy = ${data.galaxy}
-| quadrant = ${data.quadrant}
-| region = ${data.region}
-| system = ${data.system}
-| planet = ${data.planet}
-| coordinates = ${data.coordinates}
-| portalglyphs = {{Gl/Small|${data.glyphs}}}
-| government = ${data.government}
-| civsize = ${data.civSize}
-| civtype = ${data.civType}
-| founder = ${data.founder}
-| founderlink = ${data.founderLink}
-| leader = ${data.leader}
-| federation = ${data.federation}
-| un42 = ${data.un}
-| platform = ${data.platform.map((item) => item.value).join('/')}
-| release = Waypoint
-}}
-'''${data.title}''' is a civilized space community.
-
-==Summary==
-'''${data.title}''' is a [[Civilized space]] community.
-
-It is located in the [[${data.region}]] region of the [[${data.galaxy}]] galaxy.
-
-===Mission===
-${data.mission || 'Our mission is to...'}
-
-==Background==
-${data.background || 'The civilization first started...'}
-
-==Naming Guidelines==
-${data.guidelines || 'Members are encouraged to name planets/moons with...'}
-
-==Joining==
-${data.joining || 'If you want to join, you should...'}
-
-==Members==
-
-{| class="wikitable"
-! Member name !! {{platform}} !! Homeplanet !! Homesystem
-${data.members.map((member) => `|-
-| ${member.name} || ${member.platform} || ${member.homePlanet} || ${member.homeSystem}\n`).join('')}|}
-
-==Systems Discovered==
-The following systems have been documented:
-
-{{CARGOCivSystems|${data.title}}}
-
-==Rules==
-${data.rules.length > 0 ? `Rules for this civilization include:
-${data.rules.map((rule) => `* ${rule}\n`).join('')}` : ''}
-
-==Additional Information==
-${data.additionalInfo}
-
-==Gallery==
-${data.gallery.length > 0 ? `<gallery>
-${data.gallery.map((image) => {
-  return `${image.name}${image.caption ? `|${image.caption}` : ''}\n`;
-}).join('')}</gallery>` : ''}
-
-[[Category:${data.title}| ]]`;
+  const codeTemplate = civTemplate(data);
 
   return (
     <CodeView
